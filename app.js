ba98bcd4e9c1ded64ccc3eff65b709a94838e299
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-01-12 22:47:40 +0200

    JS functionality

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-//using selectors inside the element
-// traversing the dom
+//solution 1
+
+//const questionBtns = document.querySelectorAll('.question-btn');
+
+// questionBtns.forEach((btn) => {
+// 	btn.addEventListener('click', (e) => {
+// 		const question = e.currentTarget.parentElement.parentElement.classList;
+// 		question.toggle('show-text');
+// 	});
+// });
+
+//solution 2
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+	const btn = question.querySelector('.question-btn');
+	btn.addEventListener('click', () => {
+		questions.forEach((item) => {
+			if (item !== question) item.classList.remove('show-text');
+		});
+		question.classList.toggle('show-text');
+	});
+});
